a9c99678a430bb88a81b700d3bc8493fb2d928fd
Snikerso <42245963+Snikerso@users.noreply.github.com>
2023-10-26 17:29:47 +0200

    initial products list

--- src/Pages/ProductPage.tsx
+++ src/Pages/ProductPage.tsx
@@ -1,12 +1,18 @@
 import {Navigations} from '../components/Navigation'
 import {Banner} from '../components/Banner'
-
+const products = [{
+    name: "",
+    brand: "",
+    weight: 12,
+    price: 100,
+}]
 export const ProductPage = () => {
     return(
     <div>
         <Navigations></Navigations>
         <Banner></Banner>  
-
+        {/*     metoda map która pozwala każdy element listy wyświetlić osobno     */}
+        
                 
     </div>)
 }
